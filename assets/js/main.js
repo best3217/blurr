1ac898136320bbdda995b1f46cd0751fe03e1988
pdev217 <kjsv.spt@gmail.com>
2022-11-04 05:50:46 -0700

    update header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,32 @@
 
+
 gsap.registerPlugin(ScrollTrigger);
 
-ScrollTrigger.defaults({
-  scroller: '[data-scroll-container]',
-  markers: false
+
+// --- SETUP START ---
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("[data-scroll-container]"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("[data-scroll-container]", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, {duration: 0, disableLerp: true}) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("[data-scroll-container]").style.transform ? "transform" : "fixed"
 });
 
-var scroll = new LocomotiveScroll( {
-    el: document.querySelector( '[data-scroll-container]' ),
-    smooth: true,
-    multiplier: 1.0,
-    getDirection: true,
-});
-
-// Update scroll position
-scroll.on( 'scroll', ( instance ) => {
-    ScrollTrigger.update();
-    document.documentElement.setAttribute( 'data-scrolling', instance.direction );
-});
-
-// Scroll position for ScrollTrigger
-ScrollTrigger.scrollerProxy( '[data-scroll-container]', {
-    scrollTop( value ) {
-        return arguments.length ? scroll.scrollTo( value, 0, 0 ) : scroll.scroll.instance.scroll.y;
-    },
-    getBoundingClientRect() {
-        return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
-    },
-    pinType: document.querySelector( '[data-scroll-container]' ).style.transform ? "transform" : "fixed"
-} );
-
-
-ScrollTrigger.addEventListener( 'refresh', () => scroll.update() );
-ScrollTrigger.refresh();
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+ScrollTrigger.defaults({ scroller: "[data-scroll-container]" });
 
 
 let body = document.querySelector("body")
@@ -313,9 +307,9 @@ gsap.to(".parallax-img", {
   };
 
   const editCursor = e => {
-        const { clientX: x, clientY: y } = e;
-        cursor.style.left = x + 'px';
-        cursor.style.top = y + 'px';
+    const { clientX: x, clientY: y } = e;
+    cursor.style.left = x + 'px';
+    cursor.style.top = y + 'px';
   };
 
   link.forEach(b => b.addEventListener('mousemove', animateit));
